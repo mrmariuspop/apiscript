d73051b2a2938cf0e4f0d7972c8b322d4ef60476
mrmariuspop <mr.marius.pop@gmail.com>
2020-12-02 20:37:43 +0200

    upgrade version 1.3

---
+++ v1.3/src/main/java/apigenerator/apigen/ControlPanel.java
@@ -0,0 +1,22 @@
+package apigenerator.apigen;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import static apigenerator.apigen.Utils.*;
+
+public class ControlPanel {
+
+    public static void generateClasses(String sqlQuery, String entityName, String author, int nrQueryParam, Set<String> queryParams, List<String> listOfParams) throws IOException {
+
+        generateModelClass(sqlQuery, entityName, author);
+        generateQueryClass(entityName, author, sqlQuery);
+        generateDaoClass(entityName, author, nrQueryParam, listOfParams);
+        generateDaoImplClass(entityName, author, nrQueryParam, queryParams, listOfParams);
+        generateServiceClass(entityName, author, nrQueryParam,listOfParams);
+        generateServiceImplClass(entityName, author, nrQueryParam, listOfParams);
+        generateControllerClass(entityName, author, nrQueryParam, listOfParams);
+    }
+}
+
---
+++ v1.3/src/main/java/apigenerator/apigen/generators/DaoGeneratorClass.java
@@ -0,0 +1,32 @@
+package apigenerator.apigen.generators;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static apigenerator.apigen.generators.ModelGeneratorClass.getAttType;
+
+public class DaoGeneratorClass {
+
+    public static String getDaoJavaFile(String entityName, String author, int nrQueryParams, List<String> listOfParams) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MMMM/yyyy");
+        String iniialFile = "import java.util.List;\n" +
+                "\n" +
+                "/**\n" +
+                " * @author " + author + "\n" +
+                " * @since " + simpleDateFormat.format(new Date(System.currentTimeMillis())) + "\n" +
+                " */\n" +
+                "public interface " + entityName + "Dao {\n" +
+                "    List<" + entityName + "> get" + entityName + "s(";
+
+        for (int i = 1; i <= nrQueryParams; i++) {
+            iniialFile += ""+ getAttType(listOfParams.get(i-1))+" "+listOfParams.get(i-1)+"" + "" + ", ";
+        }
+
+        iniialFile = (nrQueryParams >0)?iniialFile.substring(0, iniialFile.length() - 2):iniialFile;
+
+        iniialFile += ");\n" +
+                "}";
+        return iniialFile;
+    }
+}
